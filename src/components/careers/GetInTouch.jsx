577bb7af46f95b0bfd641e23f209217ce430dc8b
arshad <arshad@sellergize.com>
2024-12-09 11:06:36 +0530

    Responsive career done

--- src/components/careers/GetInTouch.jsx
+++ src/components/careers/GetInTouch.jsx
@@ -4,7 +4,7 @@ import GetInTouchBg from "../../assets/images/career/getInTouchBg.jpg";
 const GetInTouch = () => {
     return(
         <div className="relative text-center text-white">
-            <img className="h-72 object-cover lg:h-auto lg:w-full" src={GetInTouchBg} alt="" />
+            <img className="h-72 object-cover md:h-auto md:w-full" src={GetInTouchBg} alt="" />
             <div className="absolute z-10 w-full px-4 top-1/2 start-1/2 -translate-x-1/2 lg:start-0 lg:translate-x-0 -translate-y-1/2 lg:text-start lg:ps-24">
                 <p className="text-xl lg:text-2xl lg:font-bold">Didn’t Find the Position You’re Looking For? </p>
                 <p className="text-2xl lg:text-5xl font-suez font-bold my-4 lg:my-8">Send Us Your CV</p>\
--- src/pages/CareerDetails.jsx
+++ src/pages/CareerDetails.jsx
@@ -2,31 +2,41 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import jobs from "../../public/data";
 import bg from "../assets/images/career/qa-bg.jpg";
+import bgMobile from "../assets/images/career/careerBgMobile.jpg";
 
 const CareerDetail = () => {
   const { jobRole } = useParams();
   const job = jobs.find((job) => job.position === jobRole); // Find the job that matches the role
   return (
     <div>
-      <div className="relative">
-        <img src={bg} alt="banner" />
-        <div className="absolute top-1/4 -translate-y-1/4 start-5 max-w-7xl lg:px-6 lg:start-1/2 lg:-translate-x-1/2 lg:max-w-7xl lg:w-full text-white">
+      <div className="relative pt-14 lg:pt-0">
+        <img className="hidden lg:block" src={bg} alt="banner" />
+        <img className="lg:hidden w-full " src={bgMobile} alt="banner" />
+
+        <div className="absolute top-1/2 -translate-y-1/2 lg:top-1/4 lg:-translate-y-1/4 start-5 max-w-7xl lg:px-6 lg:start-1/2 lg:-translate-x-1/2 lg:max-w-7xl lg:w-full text-white">
           <p className="text-xl font-bold">Careers</p>
-          <p className="text-5xl font-bold uppercase my-8 font-suez">{job.position}</p>
+          <p className="text-3xl lg:text-5xl font-bold uppercase my-8 font-suez">
+            {job.position}
+          </p>
           <div>
             <p className="text-xl">Location: {job.location}</p>
             <p className="text-xl">Employment Type: {job.employment_type}</p>
           </div>
-          <button className="my-8 py-4 px-8 bg-vanilla-pudding rounded-lg text-black">Apply Now</button>
+          <button className="my-8 py-4 px-8 bg-vanilla-pudding rounded-lg text-black">
+            Apply Now
+          </button>
         </div>
       </div>
-      <div className="max-w-7xl lg:mx-auto pb-16 -mt-80 relative z-10">
-        <div className="flex justify-center items-stretch">
-          <div className="w-1/2 bg-poinciana p-24 text-white">
+      <div className="max-w-7xl lg:mx-auto pb-16 lg:-mt-80 relative z-10">
+        <div className="lg:flex justify-center items-stretch">
+          <div className="w-full lg:w-1/2 bg-poinciana px-6 py-12 lg:p-24 text-white">
             <p className="text-3xl font-bold">What you will do</p>
-            <p className="mt-4 text-lg">{job.responsibilities}{job.short_description}</p>
+            <p className="mt-4 text-lg">
+              {job.responsibilities}
+              {job.short_description}
+            </p>
           </div>
-          <div className="w-1/2 bg-white p-24 ">
+          <div className="w-full lg:w-1/2 bg-white px-6 py-12 lg:p-24 ">
             <p className="text-3xl font-bold">What you will do</p>
             <p className="mt-4 text-lg">{job.who_you_are}</p>
           </div>
